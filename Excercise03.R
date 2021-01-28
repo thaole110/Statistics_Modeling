3489d50ad6ce625406a4f28299cd3b69e466b6aa
thaole110 <77394296+thaole110@users.noreply.github.com>
2021-01-28 19:04:41 +0700

    Add files via upload

--- Excercise03.R
+++ Excercise03.R
@@ -74,17 +74,17 @@ summary(M12)
 
 ##Cau6
 #Mo hinh hoi quy tuyen tinh voi nhieu bien nhat co the
-M3 <- lm(y ~ x1 + x2 + x3)
+model <- lm(y ~ x1 + x2 + x3)
 
 ##Cau7
 #Uoc luong he so hoi quy trong mo hinh tuyen tinh Cau6
-coef(M3)
+coef(model)
 #(Intercept)          x1          x2          x3 
 #32.89132428  0.80190069 -0.38136236 -0.03713244 
 
 ##Cau8
 #Tinh uoc luong V(e) va V(B)
-out <- summary(M3)
+out <- summary(model)
 #V(B0)
 se_B0 <- out$coefficients[1,2]
 se_B0
@@ -117,3 +117,60 @@ V_B3 <- (se_B3)**2
 V_B3
 #0.002706406
 
+##Tinh V(e)
+s_square <- anova_model$`Mean Sq`[4]
+s_square
+#6.744767
+#s_square la uoc luong khong chech cho V(e)
+
+##Cau 10
+#Khi them hai bien doc lap x2, x3 vao mo hinh chi co bien x1
+#chat luong uoc luong cao hon khong
+M1 <- lm(y ~ x1)
+#Bang anova cho M1
+anova(M1)
+#Analysis of Variance Table
+
+#Response: y
+#           Df Sum Sq Mean Sq F value   Pr(>F)   
+#x1         1 117.66  117.66   12.93 0.003674 **
+#Residuals 12 109.20    9.10                    
+#---
+#  Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+
+#Bang anova cho mo hinh ba bien doc lap x1, x2, x3
+anova(model)
+#Analysis of Variance Table
+#Response: y
+#           Df  Sum Sq Mean Sq F value   Pr(>F)   
+# x1         1 117.659 117.659 17.4445 0.001898 **
+# x2         1  38.314  38.314  5.6806 0.038389 * 
+# x3         1   3.436   3.436  0.5095 0.491694   
+#Residuals 10  67.448   6.745                    
+#---
+#  Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+
+SSE_H0 <- anova_M1$`Sum Sq`[2]
+SSE_H0
+#109.1983
+df_H0 <- anova_M1$Df[2]
+df_H0
+#12
+SSE_H1 <- anova_model$`Sum Sq`[4]
+SSE_H1
+#67.44767
+df_H1 <- anova_model$Df[4]
+df_H1
+#10
+r <- (df_H0 - df_H1)
+r
+#2
+F_obs <- ((SSE_H0 - SSE_H1)/r)/(SSE_H1/df_H1)
+F_obs
+#3.095036
+F_val <- qt(1-0.05,r,df_H1)
+F_obs >= F_val
+#FALSE
+#Voi muc y nghia 0.05, khong du co so bac bo H0
+#Voi muc y nghia 0.05, mo hinh hoi quy tuyen tinh voi 1 bien x1
+#co chat luong uoc luong cao hon
